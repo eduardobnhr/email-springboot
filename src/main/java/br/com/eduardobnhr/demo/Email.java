1b3b108cf42e30af7d6f68cc24109f7169d4321c
eduardobnhr <eduardobnhr@gmail.com>
2023-12-18 19:55:52 -0300

    api com a camada de servico

--- src/main/java/br/com/eduardobnhr/demo/Email.java
+++ src/main/java/br/com/eduardobnhr/demo/Email.java
@@ -1,5 +1,5 @@
 package br.com.eduardobnhr.demo;
 
-public record Email() {
+public record Email(String to, String subject, String body) {
 
 }
---
+++ src/main/java/br/com/eduardobnhr/demo/EmailController.java
@@ -0,0 +1,24 @@
+package br.com.eduardobnhr.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("email")
+public class EmailController {
+	
+	private final EmailService emailService;
+
+	public EmailController(EmailService emailService) {
+		this.emailService = emailService;
+	}
+	
+	@PostMapping
+	public void sendEmail(@RequestBody Email email) {
+		emailService.sendEmail(email);
+	
+	}
+
+}
